cf8b019eff1f236c1bf0abae131c011745ddca06
Dushmanta <dushmanta0511@gmail.com>
2024-07-21 01:27:17 +0530

    feat: add backend to monorepo

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import ChatController from "./controllers/ChatController.js";
+
+const app = express();
+dotenv.config();
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+    if (!error)
+        console.log(`Server is running on port ${PORT}`);
+    else
+        console.log("Error occurred, server can't start", error);
+});
+
+app.get("/", async (req, res) => {
+    res.end("Hello, Swasthya AI!");
+});
+
+app.get("/chat", ChatController);
